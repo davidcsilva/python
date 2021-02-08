14fddf2ee37c4f347e1350f6f27feafbf21f9b89
David Silva <david.dacostaesilva@gmail.com>
2021-02-08 16:39:53 -0300

    Adicionar exercicio 7 do cap 03

---
+++ python/cap_03/exercicio_3_7.py
@@ -0,0 +1,7 @@
+num1 = int(input("Digite o 1º numero inteiro: "))
+
+num2 = int(input("Digite o 2º numero inteiro: "))
+
+soma = num1 + num2
+
+print("A soma de %d mais %d é %d" % (num1, num2, soma ))
