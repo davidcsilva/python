8bf281677c42d317d1542cd02f4537731a307f8f
David Silva <david.dacostaesilva@gmail.com>
2021-02-08 16:24:03 -0300

    Adicionar exercicio 6 do cap 03

---
+++ python/cap_03/exercicio_3_6.py
@@ -0,0 +1,8 @@
+materia1 = float(input("Digite a nota da Materia 1: "))
+materia2 = float(input("Digite a nota da Materia 2: "))
+materia3 = float(input("Digite a nota da Materia 3: "))
+
+if((materia1 >= 7.0) and (materia2 >= 7.0) and(materia3 >= 7.0)):
+    print("Aprovado")
+else:
+    print("Reprovado")
