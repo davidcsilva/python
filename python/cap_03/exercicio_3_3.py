0a11c39642571316a989f95e0e766cd9f717d3c6
David Silva <david.dacostaesilva@gmail.com>
2021-02-08 13:05:43 -0300

    Exercicio 3 do cap 03

---
+++ python/cap_03/exercicio_3_3.py
@@ -0,0 +1,17 @@
+a = True
+b = False
+c = True
+
+print(a and a)
+print(a and b)
+print(not c)
+print(not b)
+print(not a)
+print(a and b)
+print(a and c)
+print(a or c)
+print(b or c)
+print(c or a)
+print(c or b)
+print(c or c)
+print(b or b)
