77c482749cf4352c62944d9b91b6fce84445b6f0
David Silva <david.dacostaesilva@gmail.com>
2021-02-09 11:39:26 -0300

    Adicionar modulo da aula 06

---
+++ pythonCurso/aula06/aula06.py
@@ -0,0 +1,16 @@
+'''
+Iniciar com letra, pode conter numeros, separador _, letras minusculas
+
+'''
+
+nome = 'David'
+idade = 36
+altura = 1.67
+e_maior = idade >= 18 #booleano
+data_1 = True #booleano
+data_atual = 2021
+peso = 60.0
+
+imc = peso / altura**2
+
+print('Seu IMC é: %0.2f' % imc)
