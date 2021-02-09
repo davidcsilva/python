9dc4cb9aab15855ab6961b574170f6e12879e5e8
David Silva <david.dacostaesilva@gmail.com>
2021-02-09 11:17:30 -0300

    Adicionar modulo da aula 05

---
+++ pythonCurso/aula05/aula05.py
@@ -0,0 +1,25 @@
+'''
+Operaqdores aritméticos:
+
++ : soma
+- : subtração
+* : multiplicação
+/ : divisão
+// : divisão inteira
+** : potenciação
+% : resto da divisão
+() : parẽntesis
+'''
+
+print('Multiplicação *', 10 * 10)
+print('Adição +', 10 + 10)
+print('Subtração -',10 - 5)
+print('Divisão /', 10 / 2 )
+print(10,5 // 3)
+print(10**2)
+print(10 % 3)
+print((10+2)*2)
+print(20 * 'A')
+print(20 * 2)
+print('20' + 'A')
+print(20 + 2)
