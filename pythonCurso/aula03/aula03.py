19e9562eab0a0a8b9c20e69fab216895d4985af5
David Silva <david.dacostaesilva@gmail.com>
2021-02-09 10:01:19 -0300

    Adicionar modulo da aula 03

--- pythonCurso/aula03/aula03.py
+++ pythonCurso/aula03/aula03.py
@@ -1,1 +1,13 @@
+'''
+str - String
+'''
+
 print('Olá')
+print('Alguma coisa')
+print("Aspas duplas")
+print(123456)
+print('Essa é uma string (str)')
+print("Essa é outra 'string' (str)")
+print("Esse é meu \"texto\" (str)") #caractere de escape
+print(r'Esse é meu \n (str)') # Usando r antes do texto.
+
