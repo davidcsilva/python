d12b731b6400d072dbd5a05ba107f910f4b26a2f
David Silva <david.dacostaesilva@gmail.com>
2021-02-18 20:06:21 -0300

    Adicionando novos arquivos com melhorias

--- python/cap_05/exercicio_5_004.py
+++ python/cap_05/exercicio_5_004.py
@@ -1,4 +1,30 @@
-fim = int(input('Digite um numero para contagem: '))
+import re
+ 
+def is_float(val):
+    if isinstance(val, float): return True
+    if re.search(r'^\-{,1}[0-9]+\.{1}[0-9]+$', val): return True
+ 
+    return False
+ 
+def is_int(val):
+    if isinstance(val, int): return True
+    if re.search(r'^\-{,1}[0-9]+$', val): return True
+ 
+    return False
+ 
+def is_number(val):
+    return is_int(val) or is_float(val)
+
+ 
+e_num = False
+while e_num != True:
+    fim = input('Digite um numero para contagem: ')
+    e_num = is_int(fim)
+    if e_num != True:
+        print('Digite um valor válido!')
+
+fim = int(fim)
+
 x  = 0
 while (x <= fim):
     if (x % 2 != 0):
