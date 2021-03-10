4885cf7cc8faa0c88e981c00483f7ec463b84826
David Silva <david.dacostaesilva@gmail.com>
2021-03-10 14:04:22 -0300

    Atualizar bibliotecas python; Numpy, Scipy, Pandas...

--- DataCamp/Numpy/env/Scripts/activate.bat
+++ DataCamp/Numpy/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\David\Meus Documentos\git\python\DataCamp\Numpy\env
+set VIRTUAL_ENV=C:\Users\David\Documents\git\python\DataCamp\Numpy\env
 
 if not defined PROMPT set PROMPT=$P$G
 
