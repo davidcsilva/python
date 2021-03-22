f3d3a3d27be19646be756d78a85ad8a22159279f
David Silva <david.dacostaesilva@gmail.com>
2021-03-22 18:56:08 -0300

    alteração do exercicio_5_023.py do cap 05

--- python/cap_05/exercicio_5_023.py
+++ python/cap_05/exercicio_5_023.py
@@ -1,27 +1,12 @@
-num = input('Digite um número: ')
+n = int(input("Verificar numeros primos ate: "))
+mult=0
 
-e_num = num.isdigit()
-e_primo = False
-if e_num == True:
-    num = int(num)
-    if num % 2 == 0:
-        e_primo = False
-    else:
-        for i in range(1, num, 2):
-            print(i)
-            if num == 2:
-                e_primo = True
-            if num == 3:
-                e_primo = True
-            if num % i != 0:
-                e_primo = True
-            else:
-                e_primo = False
-        else:
-            if e_primo == False:
-                print(f'{num} não é primo')
-            else:
-                print(f'{num} é primo')
+for count in range(2,n):
+    if (n % count == 0):
+        print("Múltiplo de",count)
+        mult += 1
 
+if(mult==0):
+    print("É primo")
 else:
-    print('Valor digitado é inválido!')
+    print("Tem",mult," múltiplos acima de 2 e abaixo de",n)
